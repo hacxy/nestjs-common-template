bf73438596488cb3b72e802657abe2adb51cb293
Hacxy <hacxy.97@outlook.com>
2024-05-14 17:01:28 +0800

    feat: 新增token校验

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,20 +1,20 @@
 import { ApiProperty } from 'uni-nest';
 
-export class UserDto {
+export class CreateUserDto {
   @ApiProperty({
     description: '用户名',
     example: 'admin',
-    required: true,
+    required: true
   })
   username: string;
 
   @ApiProperty({
-    description: '手机号',
+    description: '手机号'
   })
   mobile: string;
 
   @ApiProperty({
-    description: '邮箱',
+    description: '邮箱'
   })
   email: string;
 }
